cb6294d4d799d8c41a6f4e6f1d23209bfcad3990
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-05-21 16:53:50 +0200

    Create index.php

---
+++ jour01/job03/index.php
@@ -0,0 +1,10 @@
+
+<?php
+    echo "<pre>";
+//Déclarer un tablea
+$tableau=["Bonjour", 15, true, 58.15]; //Chaine de caractères, entier, bouléen, décimal
+var_dump($tableau);
+    echo "</pre>";
+?>
+
+
