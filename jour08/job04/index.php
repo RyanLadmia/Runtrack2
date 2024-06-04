74c17c6c103afff278bb1d0286f93500e5dea9e7
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:09:26 +0200

    Create index.php

---
+++ jour08/job04/index.php
@@ -0,0 +1,36 @@
+<?php
+    // Intro Cookie :
+    // Durée de vie du cookie : 1 heure (3600 secondes)
+    $cookie_duration = 36000;
+
+
+    // Recuperation des champs du formulaire :
+
+
+    // Ajouter Prenom dans cookie :
+
+    
+    // Affichage du message de bienvenue + form deconnexion :
+
+
+    // Deconnexion :
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 8; Job 4</title>
+</head>
+<body>
+    <form>
+        <label type="text" name="prenom">Prenom</label>
+        <input type="text" name="prenom" id="prenom">
+        <input type="submit" name="Connexion" value="Connexion">
+    </form>
+    
+</body>
+</html>
