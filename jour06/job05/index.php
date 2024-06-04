0487fd1069d7e6f1b315a1e06fc11dd871e328a1
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:36 +0200

    Create index.php

---
+++ jour06/job05/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire avec styles</title>
+    <?php
+        if (isset($_POST['style'])) {
+            $selectstyle = $_POST['style'];
+            $validstyles = ['style1', 'style2', 'style3'];
+
+            foreach ($validstyles as $style) {
+                if ($selectstyle === $style) {
+                    echo '<link rel="stylesheet" type="text/css" href="' . $selectstyle . '.css">';
+                    break;
+                }
+            }
+        }
+    ?>
+</head>
+
+<body>
+    <form method="POST" action="index.php">
+        <label for="style">Choisissez un style :</label>
+        <select name="style" id="style">
+            <option value="style1">Style 1</option>
+            <option value="style2">Style 2</option>
+            <option value="style3">Style 3</option>
+        </select><br>
+        <input class="bouton" type="submit" value="Appliquer le style">
+    </form>
+</body>
+</html>
