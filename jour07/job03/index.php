964a4e4f175ccac0ded7d4250a879447036a15fa
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:09:06 +0200

    Create index.php

---
+++ jour07/job03/index.php
@@ -0,0 +1,22 @@
+<?php
+    function getHello(){
+        return "Hello LaPlateforme!";
+    }
+    $text= getHello();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour7 Job3</title>
+</head>
+<body>
+    <?php
+        echo $text;
+    ?>
+    
+</body>
+</html>
