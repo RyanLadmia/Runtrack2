24e9e4b338cc0b7a1c8debec5c3f10b420ce7745
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:10:34 +0200

    Create index.php

---
+++ jour10/job09/index.php
@@ -0,0 +1,25 @@
+<?php
+    $mysqli = mysqli_connect("localhost", "root", "", "jour09");
+    $myresult = mysqli_query($mysqli, "SELECT * FROM salles ORDER BY capacite DESC");
+    $row = mysqli_fetch_assoc($myresult);
+
+    echo
+    "<table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Id_etage</th>
+                <th>Capacite</th>
+            </tr>
+        </thead>
+        <tbody>";
+            foreach($myresult as $user){
+                echo "<tr>";
+                    echo "<td>".$user['nom']."</td>";
+                    echo "<td>".$user['id_etage']."</td>";
+                    echo "<td>".$user['capacite']."</td>";
+                echo "</tr>";
+            }
+            echo "</tbody>
+    </table>";
+?>
