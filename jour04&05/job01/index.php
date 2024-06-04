9e4b72ed696e7a64996ddfd82827eac4d16da48c
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:28 +0200

    Create index.php

---
+++ jour04&05/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="GET">
+        <label for="name">Nom</label>
+        <input type="text" name="name" id="name">
+        <label for="firstname">Prénom</label>
+        <input type="text" name="firstname" id="firstname">
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+        
+    ?>
+</body>
+</html>
