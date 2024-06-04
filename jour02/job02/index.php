d8cc062ead08db1f90c74a554da4701f63918166
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:47 +0200

    Update index.php

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -8,12 +8,23 @@
 
 <body>
     <?php
-    $exclusions = [26, 37, 88, 1111, 3233];
-        for ($i=0; $i<=1337; $i++){
-            if (!in_array($i, $exclusions)) {
-                echo $i . '<br />';
+    /* Avec Fonction système !array_:
+    
+        $exclusions = [26, 37, 88, 1111, 3233];
+            for ($i=0; $i<=1337; $i++){
+                if (!array_($i, $exclusions)) {
+                    echo $i . '<br />';
         }
+        }*/
+
+        for($i=0; $i<=1337; $i++){
+            if($i==26 ||$i== 37 ||$i== 88||$i== 1111||$i== 3233){
+                echo null;
+            } else {
+                echo $i.'<br/>';
+            }
         }
+
     ?>
 </body>
 </html>
