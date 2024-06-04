f26768445edd34b018219d47580c3cb4a805f73b
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:17 +0200

    Create index.php

---
+++ jour02/job07/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangle</title>
+</head>
+
+<body>
+    <?php
+       $hauteur = 5; 
+
+       for($h=0; $h<$hauteur;$h++){
+            if($h=0|| $h<$hauteur -1){
+                echo "/";
+            }
+        for($h=0 || $h)
+       }
+
+       
+    ?>
+</body>
+</html>
