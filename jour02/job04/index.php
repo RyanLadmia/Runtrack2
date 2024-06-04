4dc98506d324761edb7e0eaca685e74b65dc2e41
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:52 +0200

    Update index.php

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,11 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jour2 Job04</title>
 </head>
 <body>
     <?php
-        for ($i=1; $i<=100; $i++){
+    
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--for ($i=1; $i<=100; $i++){
             if ($i % 3==0 && $i % 5==0) {
                 echo "FizzBuzz<br/>";
 
@@ -20,8 +44,55 @@
             } else {
                 echo $i . "<br/>";
             }       
-        }
-    ?>
+        } 
+
+
+
+
+
+    
+    // Creer un tableau vide
+$tabNotes = array();
+
+// Ajouter de nouvelles valeurs a un tableau
+$tabNotes[] = 12.5;
+$tabNotes[] = 9;
+
+// Afficher les valeurs
+echo 'La valeur à l\'indice 0 est:'. $tabNotes[0].'<br>';
+echo 'La valeur à l\'indice 1 est:'. $tabNotes[1].'<br>';
+
+// Ajouter une autre note
+$tabNotes[] = 11;
+
+// Changer une note puis afficher la nouvelle valeur
+$tabNotes [1] = 8;
+echo 'La valeur à l\'indice 1 est:'. $tabNotes[1];
+
+
+
+
+
+
+    
+//MANIPULER UN TABLEAU SANS EN CONNAÎTRE LA TAILLE :
+// Creer un tableau rempli
+$tabNotes = array (12.5 , 9 , 11 , 7 , 12.5);
+
+$somme = 0; // initialiser une variable
+
+
+// Parcourir un tableau
+for ($i = 0; $i < count($tabNotes); $i ++)
+{
+ $somme= $somme + $tabNotes[$i];
+}
+
+// Calculer la moyenne
+$moyenne = $somme / count($tabNotes);
+echo '<p>La moyenne vaut :'. $moyenne .'</p>';
+    -->
+
 
 </body>
 </html>
