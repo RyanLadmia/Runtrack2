01b5a0ad33fe8bc98e96cc50d99aaa33cbe00490
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:10:40 +0200

    Create index.php

---
+++ jour10/job11/index.php
@@ -0,0 +1,22 @@
+<?php
+    $mysqli = mysqli_connect("localhost", "root", "", "jour09");
+    $myresult = mysqli_query($mysqli, "SELECT AVG(capacite) AS capacite FROM salles");
+    $row = ($myresult);
+
+    echo
+    "<table>
+        <thead>
+            <tr>
+                <th>Capacite</th>
+            </tr>
+        </thead>
+        <tbody>";
+            foreach($myresult as $user){
+                echo "<tr>";
+                    echo "<td>".$user['capacite']."</td>";
+                echo "</tr>";
+            }
+            echo "</tbody>
+    </table>";
+
+?>
