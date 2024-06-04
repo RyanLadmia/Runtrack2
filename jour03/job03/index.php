67c083ae532e7a9f0cca7e58c7c551641f79841b
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-04 16:08:05 +0200

    Create index.php

---
+++ jour03/job03/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voyelles</title>
+</head>
+<body>
+    <?php
+    
+    $str = "I'm sorry Dave I'm afraid I can't do that";
+
+    for($i=0; $i < strlen($str);$i++){
+            $chara = $str[$i];
+
+            if($chara=='a' ||$chara=='A'||$chara=='e'||$chara=='E'||$chara=='i'||$chara=='I'||$chara=='o'||$chara=='O'||$chara=='u'||$chara=='U'||$chara== 'y'||$chara=='Y')
+                echo $chara;
+        }
+      
+    ?>    
+
+
+</body>
+</html>
