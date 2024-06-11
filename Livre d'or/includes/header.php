fe07cfc54675362c19fe8975e57be4542b5bb891
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-11 14:21:23 +0200

    Create header.php

---
+++ Livre d'or/includes/header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="livre-or.php">Livre d'or</a></li>
+            <li><a href="commentaire.php">Commentaire</a></li>
+            <li><a href="inscriptionGPT.php">GPT</a></li>
+
+        </ul>
+    </nav>
+</header>
