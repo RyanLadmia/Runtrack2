979166d5a79d55e6069ed39528f967ad6329ca1a
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-11 14:21:33 +0200

    Create index.php

---
+++ Livre d'or/assets/pages/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+
+<body>
+       <!-- Header -->
+<?php include'../../includes/header.php'?>
+    <main>
+    <p>Bienvenue sur mon site.</p>
+    <a class="bouton" href="deconnexion.php">Déconnexion</a>
+    </main>
+</body>
+</html>
