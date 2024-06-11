7600a1ba1449d077e34f84c69865f4c924194e2a
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-11 14:21:02 +0200

    Create index.php

---
+++ Module de connexion/assets/pages/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href='../../assets/css/index.css'>
+</head>
+
+<body>
+<?php include'../../includes/header.php'?>
+
+<h1>&nbsp Bienvenue sur ce site !&nbsp</h1>
+
+</body>
+</html>
