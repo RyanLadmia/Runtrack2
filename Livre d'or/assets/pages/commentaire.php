4fc293c9bfd8115a058c23e8567d9dfe2c1f6d70
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-11 14:21:40 +0200

    Create commentaire.php

---
+++ Livre d'or/assets/pages/commentaire.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nouveau commentaire</title>
+</head>
+<body>
+    <!-- Header -->
+    <?php include '../../includes/header.php'; ?>
+    
+</body>
+</html>
