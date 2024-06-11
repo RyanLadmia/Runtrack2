4a896b4a186f51ddd3fd4bde7f0c932e29dac13e
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-11 14:20:56 +0200

    Create header.php

---
+++ Module de connexion/includes/header.php
@@ -0,0 +1,11 @@
+<header>
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="profil.php">Profil</a></li>
+            <li><a href="admin.php">Admin</a></li>
+        </ul>
+    </nav>
+</header>
