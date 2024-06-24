78e5269a8d934bad8c5bb59bb0d4d3bd1fd50c97
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:26:58 +0200

    Update header.php

--- Livre d'or/includes/header.php
+++ Livre d'or/includes/header.php
@@ -1,14 +1,12 @@
 <header>
     <nav class="navbar">
         <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="inscription.php">Inscription</a></li>
-            <li><a href="connexion.php">Connexion</a></li>
-            <li><a href="profil.php">Profil</a></li>
-            <li><a href="livre-or.php">Livre d'or</a></li>
-            <li><a href="commentaire.php">Commentaire</a></li>
-            <li><a href="inscriptionGPT.php">GPT</a></li>
-
+            <li class="header"><a href="index.php">Accueil</a></li>
+            <li class="header"><a href="inscription.php">Inscription</a></li>
+            <li class="header"><a href="connexion.php">Connexion</a></li>
+            <li class="header"><a href="profil.php">Profil</a></li>
+            <li class="header"><a href="livre-or.php">Livre d'or</a></li>
+            <li class="header"><a href="commentaire.php">Commentaire</a></li>
         </ul>
     </nav>
 </header>
