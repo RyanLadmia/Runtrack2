f0073564bc1e290d6a70560cd7acc15232e4b092
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:26:22 +0200

    Create index.php

---
+++ jour06/job04/job05/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 4&5 job 5</title>
+</head>
+<body>
+    <br><br>
+    <form method="POST" action="index.php">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Est-ce votre ultime bafouille?">
+    </form><br><br>
+</body>
+
+    <?php
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = isset($_POST['username']) ? $_POST['username'] : '';
+        $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+        if ($username === "John" && $password === "Rambo") {
+            echo "<p>C'est pas ma guerre</p>";
+        } else {
+            echo "<p>Votre pire cauchemar</p>";
+        }
+    }
+    ?>
+</html>
