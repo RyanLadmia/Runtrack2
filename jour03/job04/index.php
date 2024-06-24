ef117701f0ba2e6ef8e8e6043b70cb5052e1d710
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:09 +0200

    Update index.php

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Couter characters</title>
+    <title>Jour 3 Job 4</title>
 </head>
 
 <body>
