d7d25cfa14d148ca00f15f380ef21fadc83c1df5
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:27:14 +0200

    Update index.php

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour2 Job04</title>
+    <title>Jour2 Job 4</title>
 </head>
 <body>
     <?php
-    
+        for($i=1 ; $i<=100; $i++){
+            if ($i % 3 == 0 && $i % 5 == 0){
+                echo "FizzBuzz<br/>";
+            } elseif ($i % 5 == 0){
+                echo "Buzz<br/>";
+            } elseif ($i % 3 ==0){
+                echo"Fizz<br/>";
+            } else {
+                echo $i."<br/>";
+            }
+        }
 
     ?>
 
