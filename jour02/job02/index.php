fa67e5bc08e46454da1dd862b3573af962677446
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:27:20 +0200

    Update index.php

--- jour02/job02/index.php
+++ jour02/job02/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jour 2 Job 2</title>
 </head>
 
 <body>
