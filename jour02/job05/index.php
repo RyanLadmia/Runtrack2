bb92dda49e62c148af5b78f030af80a272c7996e
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:27:11 +0200

    Update index.php

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -3,30 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jour 2 Job 5</title>
 </head>
 
 <body>
-    <?php
-        function estPremier($nombre) {
-            if ($nombre <= 1) {
-                return false;
-            }
-            for ($i = 2; $i <= sqrt($nombre); $i++) {
-                if ($nombre % $i == 0) {
-                    return false;
-                }
-            }
-            return true;
+<?php
+// Boucle principale pour parcourir les nombres de 2 à 1000
+for ($num = 2; $num <= 1000; $num++) {
+    $is_prime = true; // Initialisation d'un booléen pour vérifier si le nombre est premier
+    // Vérification des diviseurs possibles du nombre courant
+    for ($i = 2; $i * $i <= $num; $i++) {
+        if ($num % $i == 0) {
+            $is_prime = false; // Le nombre n'est pas premier
+            break; // Sortir de la boucle si un diviseur est trouvé
         }
+    }
+    // Si le nombre est premier, l'afficher
+    if ($is_prime) {
+        echo $num . "<br />";
+    }
+}
+?>
 
-        for ($i = 2; $i <= 1000; $i++) {
-            if (estPremier($i)) {
-                echo $i . "<br />";
-            }
-        }
 
 
-    ?>
+
+    
 </body>
 </html>
