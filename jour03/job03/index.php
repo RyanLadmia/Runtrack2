37e4e635cf3d9dd8b4a339513ea2a4d6aa80a6fd
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:01 +0200

    Update index.php

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voyelles</title>
+    <title>Jour 3 Job 3</title>
 </head>
 <body>
     <?php
     
     $str = "I'm sorry Dave I'm afraid I can't do that";
 
-    for($i=0; $i < strlen($str);$i++){
+    for ($i = 0; isset($str[$i]); $i++) {
             $chara = $str[$i];
 
             if($chara=='a' ||$chara=='A'||$chara=='e'||$chara=='E'||$chara=='i'||$chara=='I'||$chara=='o'||$chara=='O'||$chara=='u'||$chara=='U'||$chara== 'y'||$chara=='Y')
