df0e005a3e50e72a36f01bf8a0d34b4a1e96c84c
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:25:36 +0200

    Update index.php

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -3,21 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Triangle</title>
+    <title>Jour 2 job 7</title>
 </head>
 
 <body>
-    <?php
-       $hauteur = 5; 
 
-       for($h=0; $h<$hauteur;$h++){
-            if($h=0|| $h<$hauteur -1){
-                echo "/";
-            }
-        for($h=0 || $h)
-       }
+<?php
 
-       
-    ?>
+$height = 5; // Set the desired height of the triangle
+
+for ($row = 0; $row < $height; $row++) {
+    // Print leading spaces
+    for ($space = 0; $space < $height - $row - 1; $space++) {
+        echo " &nbsp;";
+    }
+    // Print the left side of the triangle
+    echo " /";
+
+    // Print spaces or underscores for the base
+    if ($row == $height - 1) {
+        for ($col = 0; $col < 2 * $row; $col++) {
+            echo "_";
+        }
+    } else {
+        for ($col = 0; $col < 2 * $row; $col++) {
+            echo "&nbsp; ";
+        }
+    }
+
+    // Print the right side of the triangle
+    if ($row > -1) {
+        echo "\\";
+    }
+    echo "<br />"; // Move to the next line after each row
+}
+
+?>
 </body>
 </html>
