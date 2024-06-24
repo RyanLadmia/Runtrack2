a5f762827bf95623539ea0bbf8c95f888756594a
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:25 +0200

    Update index.php

--- jour04&05/job01/index.php
+++ jour04&05/job01/index.php
@@ -1,22 +1,31 @@
+<?php
+$counter = 0;
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    foreach ($_GET as $key => $value) {
+        if (isset($_GET[$key]) && $_GET[$key] !== '') {
+            $counter++;
+        }
+    }  
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jour 4 job 1</title>
 </head>
 
 <body>
     <form action="index.php" method="GET">
-        <label for="name">Nom</label>
-        <input type="text" name="name" id="name">
-        <label for="firstname">Prénom</label>
-        <input type="text" name="firstname" id="firstname">
+        <label for="name">Nom</label><br>
+        <input type="text" name="name" id="name"><br><br>
+        <label for="firstname">Prénom</label><br>
+        <input type="text" name="firstname" id="firstname"><br><br>
         <input type="submit" value="Envoyer">
     </form>
-
-    <?php
-        
-    ?>
+    <br><br>
+    <?php echo "Le nombre d'arguments GET envoyés est : " . $counter; ?>
 </body>
 </html>
