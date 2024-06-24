86d9e1ea7557549788ef58f260674ab13e6e3950
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:07 +0200

    Update index.php

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau Paire ou Impaire</title>
+    <title>Jour 3 Job 1</title>
 </head>
 <body>
     <?php
