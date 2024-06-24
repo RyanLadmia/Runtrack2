e61c3a9427cdc9cf97db9aa55c2fa851890c1f5a
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:19 +0200

    Update index.php

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -3,16 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Jour 3 Job 7</title>
 </head>
 
 <body>
-    <?php
-    $str = "Certaines choses changent, et d'autres ne changeront jamais.";
-    $srt2 = "";
+<?php
+  
+$str = "Certaines choses changent, et d'autres ne changeront jamais.";
+$str2 = "";
+
+// Parcourir la chaîne en remplaçant les caractères
+for ($i = 0; isset($str[$i + 1]); $i++) {
+    $str2 .= $str[$i + 1];
+}
+
+// Ajouter le premier caractère à la fin
+$str2 .= $str[0];
+
+// Afficher le résultat
+echo $str2;
+?>
 
     
-    ?>
     
 </body>
 </html>
