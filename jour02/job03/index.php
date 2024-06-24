a8bcf461d4730a9786a2ff876d3ffb2a20899ac7
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:27:17 +0200

    Update index.php

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -8,7 +8,17 @@
 
 <body>
     <?php
-      
+      for ($i=0; $i<=100; $i++){
+        if($i == 42){
+            echo "La Plateforme_<br/>";
+        } elseif ($i>=25 && $i<=50){
+         echo "<u>$i</u><br/>";
+        } elseif($i>=0 && $i<=20) {
+            echo "<i>$i</i><br/>";
+        } else{
+            echo $i.'<br/>';
+        }
+      }
     
     ?>
 
