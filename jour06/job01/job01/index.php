242a4a2d7e68a7eacb5a00f5a3d3873356cdb07c
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:25:21 +0200

    Create index.php

---
+++ jour06/job01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+    $str = "LaPlateforme";
+    $str2 = "Vive ";
+    $str3 ="!";
+        echo $str2.$str.$str3;
+        echo "<br>";
+?>
+
+<?php
+$val = 6;
+    echo $val."<br>";
+    echo $val+4;
+    echo "<br>";
+?>
+
+<?php
+$myBool = "true";
+    echo $myBool;
+    echo "<br>";
+$myBool2 = "false";
+    echo $myBool2;
+?>
