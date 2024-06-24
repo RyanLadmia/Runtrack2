8a5e97f9a40c6ee3488d90905b72eee3714e93be
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:24:58 +0200

    Create index.php

---
+++ jour06/job02/job04/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour2 Job 4</title>
+</head>
+<body>
+    <?php
+        for($i=1 ; $i<=100; $i++){
+            if ($i % 3 == 0 && $i % 5 == 0){
+                echo "FizzBuzz<br/>";
+            } elseif ($i % 5 == 0){
+                echo "Buzz<br/>";
+            } elseif ($i % 3 ==0){
+                echo"Fizz<br/>";
+            } else {
+                echo $i."<br/>";
+            }
+        }
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--for ($i=1; $i<=100; $i++){
+            if ($i % 3==0 && $i % 5==0) {
+                echo "FizzBuzz<br/>";
+
+            } elseif ($i % 3==0) {
+                echo "Fizz<br/>";
+
+            } elseif ($i % 5==0) {
+                echo "Buzz<br/>";
+
+            } else {
+                echo $i . "<br/>";
+            }       
+        } 
+
+
+
+
+
+    
+    // Creer un tableau vide
+$tabNotes = array();
+
+// Ajouter de nouvelles valeurs a un tableau
+$tabNotes[] = 12.5;
+$tabNotes[] = 9;
+
+// Afficher les valeurs
+echo 'La valeur à l\'indice 0 est:'. $tabNotes[0].'<br>';
+echo 'La valeur à l\'indice 1 est:'. $tabNotes[1].'<br>';
+
+// Ajouter une autre note
+$tabNotes[] = 11;
+
+// Changer une note puis afficher la nouvelle valeur
+$tabNotes [1] = 8;
+echo 'La valeur à l\'indice 1 est:'. $tabNotes[1];
+
+
+
+
+
+
+    
+//MANIPULER UN TABLEAU SANS EN CONNAÎTRE LA TAILLE :
+// Creer un tableau rempli
+$tabNotes = array (12.5 , 9 , 11 , 7 , 12.5);
+
+$somme = 0; // initialiser une variable
+
+
+// Parcourir un tableau
+for ($i = 0; $i < count($tabNotes); $i ++)
+{
+ $somme= $somme + $tabNotes[$i];
+}
+
+// Calculer la moyenne
+$moyenne = $somme / count($tabNotes);
+echo '<p>La moyenne vaut :'. $moyenne .'</p>';
+    -->
+
+
+</body>
+</html>
