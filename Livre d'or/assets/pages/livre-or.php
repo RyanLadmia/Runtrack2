cca323ff2e1a9e278c3c09cea372953114dba43d
ryan-ladmia <ryan.ladmia@laplateforme.io>
2024-06-24 10:26:49 +0200

    Update livre-or.php

--- Livre d'or/assets/pages/livre-or.php
+++ Livre d'or/assets/pages/livre-or.php
@@ -8,7 +8,7 @@
         }
 
     // Requete sql pour afficher les commentaires :
-    $sql = "SELECT utilisateurs.login, commentaires.date, commentaires.commentaire FROM utilisateurs INNER JOIN commentaires";
+    $sql = "SELECT utilisateurs.login, commentaires.date, commentaires.commentaire FROM utilisateurs INNER JOIN commentaires ON utilisateurs.id = commentaires.id_utilisateur ORDER BY commentaires.date DESC";
     $result = $conn->query($sql);
 
     $comments = [];
@@ -26,25 +26,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Espace commentaires</title>
+    <link rel="stylesheet" href='../../assets/css/livre_or.css'>
+    <link rel="icon" type="image/x-icon" href='../../assets/medias/livre_or_favicon.jpg'>
 </head>
 <body>
     <!-- Header -->
     <?php include '../../includes/header.php'; ?>
     <main>
+    <h2 class="h2_livre-or">Commentaires des utilisateurs :</h2>
+        <br><br>
         <?php if(isset($_SESSION['loggedin'])&&$_SESSION['loggedin']===true):?>
-            <a href="commentaire.php">Ajouter un commentaire</a>
+            <a class="ajout" href="commentaire.php"><u>Ajouter un commentaire :</u></a>
         <?php endif;?>
+        <br><br>
 
         <?php if(!empty($comments)):?>
             <?php foreach($comments as $comment):?>
                 <div class="comment">
-                    <p>Posté le <?php echo date('d/m/Y', strtotime($comment['date'])); ?> par <?php echo htmlspecialchars($comment['login']);?>;</p>
+                    <p>Posté le <?php echo date('d/m/Y', strtotime($comment['date'])); ?> par <?php echo htmlspecialchars($comment['login']);?> :</p>
                     <p> <?php echo htmlspecialchars($comment['commentaire']); ?></p>
                 </div>
             <?php endforeach;?>
             <?php else:?>
                 <p>Aucun commentaire pour le moment.</p>
         <?php endif;?>
+        <a class="bouton_livre-or" href="deconnexion.php">Déconnexion</a>
     </main>
 </body>
 </html>
